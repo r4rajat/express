72a4350748dff39bdbea9448d5bb1a4fa0176cf0
Rajat Gupta <rajat.11605708@lpu.in>
2022-06-21 20:51:00 +0530

    Created Controller

---
+++ controller.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	corev1 "k8s.io/api/core/v1"
+	netv1 "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	appsinformer "k8s.io/client-go/informers/apps/v1"
+	"k8s.io/client-go/kubernetes"
+	appslisters "k8s.io/client-go/listers/apps/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+	"log"
+	"os"
+	"time"
+)
+
+type controller struct {
+	clientSet           kubernetes.Interface
+	deploymentLister    appslisters.DeploymentLister
+	deploymentCacheSync cache.InformerSynced
+	queue               workqueue.RateLimitingInterface
+}
+
+func getNewController(clientSet kubernetes.Interface, deploymentInformer appsinformer.DeploymentInformer) *controller {
+	queue := os.Getenv("EXPRESS_QUEUE")
+	if queue == "" {
+		queue = "EXPRESS"
+	}
+	newController := &controller{
+		clientSet:           clientSet,
+		deploymentLister:    deploymentInformer.Lister(),
+		deploymentCacheSync: deploymentInformer.Informer().HasSynced,
+		queue:               workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), queue),
+	}
+	deploymentInformer.Informer().AddEventHandler(
+		cache.ResourceEventHandlerFuncs{
+			AddFunc:    newController.addHandler,
+			DeleteFunc: newController.deleteHandler,
+		},
+	)
+	return newController
+}
+
+func (c *controller) deleteHandler(obj interface{}) {
+	log.Println("Delete Handler Called")
+	c.queue.Add(obj)
+}
+
+func (c *controller) addHandler(obj interface{}) {
+	log.Println("Add Handler Called")
+	c.queue.Add(obj)
+}
+
+func (c *controller) run(ch <-chan struct{}) {
+	if !cache.WaitForCacheSync(ch, c.deploymentCacheSync) {
+		log.Println("Waiting for cache to be Synced.")
+	}
+	go wait.Until(c.worker, 1*time.Second, ch)
+	<-ch
+}
+
+func (c *controller) worker() {
+	for c.processItems() {
+
+	}
+}
+
+func (c *controller) processItems() bool {
+	item, shutdown := c.queue.Get()
+	if shutdown {
+		return false
+	}
+	defer c.queue.Forget(item)
+	key, err := cache.MetaNamespaceKeyFunc(item)
+	if err != nil {
+		log.Printf("Error getting key from cache.\nreason --> %s", err.Error())
+	}
+
+	ns, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		log.Printf("Error splitting key into namespace and name.\nReason --> %s", err.Error())
+		return false
+	}
+
+	err = c.createService(ns, name)
+	if err != nil {
+		//retry
+		log.Printf("Error Syncing Deployments.\nReason --> %s", err.Error())
+		return false
+	}
+	return true
+}
+
+func (c *controller) createService(ns string, name string) error {
+	//create service
+	deployment, err := c.deploymentLister.Deployments(ns).Get(name)
+	if err != nil {
+		log.Printf("Error Getting Deployment Name from Lister.\nReason --> %s", err.Error())
+	}
+	svc := corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      deployment.Name,
+			Namespace: ns,
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: deployment.Spec.Template.Labels,
+			Ports: []corev1.ServicePort{
+				{
+					Name: "http",
+					Port: 80,
+				},
+				{
+					Name: "https",
+					Port: 443,
+				},
+			},
+		},
+	}
+	ctx := context.Background()
+	s, err := c.clientSet.CoreV1().Services(ns).Create(ctx, &svc, metav1.CreateOptions{})
+	if err != nil {
+		log.Printf("Error Creating Service.\nReason --> %s", err.Error())
+	}
+	log.Println("Service Created... ", s)
+	//create ingress
+	return createIngressForService(ctx, c.clientSet, svc)
+}
+
+func createIngressForService(ctx context.Context, client kubernetes.Interface, service corev1.Service) error {
+	pathType := "Prefix"
+	ingress := netv1.Ingress{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      service.Name,
+			Namespace: service.Namespace,
+			Annotations: map[string]string{
+				"nginx.ingress.kubernetes.io/rewrite-target": "/",
+			},
+		},
+		Spec: netv1.IngressSpec{
+			Rules: []netv1.IngressRule{
+				{
+					IngressRuleValue: netv1.IngressRuleValue{
+						HTTP: &netv1.HTTPIngressRuleValue{
+							Paths: []netv1.HTTPIngressPath{
+								{
+									Path:     fmt.Sprintf("/%s", service.Name),
+									PathType: (*netv1.PathType)(&pathType),
+									Backend: netv1.IngressBackend{
+										Service: &netv1.IngressServiceBackend{
+											Name: service.Name,
+											Port: netv1.ServiceBackendPort{
+												Number: 80,
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	_, err := client.NetworkingV1().Ingresses(service.Namespace).Create(ctx, &ingress, metav1.CreateOptions{})
+	return err
+}
--- main.go
+++ main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"github.com/mitchellh/go-homedir"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"log"
+	"time"
+)
 
+func main() {
+	homeDir, err := homedir.Dir()
+	if err != nil {
+		log.Printf("Unable to get Home Directory of Current User.\nReason --> %s", err.Error())
+	}
+	kubeconfigPath := homeDir + "/.kube/config"
+	log.Printf("Setting default kubeconfig location to --> %s", kubeconfigPath)
+	kubeconfig := flag.String("kubeconfig", kubeconfigPath, "Location to your kubeconfig file")
+	if (*kubeconfig != "") && (*kubeconfig != kubeconfigPath) {
+		log.Printf("Recieved new kubeconfig location --> %s", *kubeconfig)
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		log.Printf("Not able to create kubeconfig object from default location.\nReason --> %s", err.Error())
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			log.Printf("Not able to create kubeconfig object from inside pod.\nReason --> %s", err.Error())
+		}
+	}
+	log.Println("Created config object with provided kubeconfig")
+
+	// Creating Clientset
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("Error occurred while creating Client Set with provided config.\nReason --> %s", err.Error())
+	}
+	ch := make(chan struct{})
+	informer := informers.NewSharedInformerFactory(clientSet, 10*time.Minute)
+	controller := getNewController(clientSet, informer.Apps().V1().Deployments())
+	informer.Start(ch)
+	controller.run(ch)
+	log.Println("Informer --> ", informer)
 }
